156d7e78d210d2619a6fead9c36a9437f1b40d9f
Junior <sudojunior@outlook.com>
2021-02-02 20:03:03 +0000

    add internal type definitions, unsure if they'll ever be used.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,13 @@
 // Internal
 
+/*
+declare function error<Value>(origin: string, arg: string, value: Value, types: any[]): void
+declare function signal<Value>(value: Value, type: any): boolean
+declare function multi<Values>(values: Values, types: any[]): boolean
+declare function check<Value>(value: Value, types: any[], options?: ValidateOptions): boolean
+declare function condition<Value>(value: Value, result: boolean, options?: ValidateOptions): boolean
+*/
+
 // Exported
 
 interface ValidateOptions {
